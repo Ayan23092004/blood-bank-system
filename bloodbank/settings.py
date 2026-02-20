aa428d1f315344138a15301e37b53f7c9cf97044
Ayan23092004 <ayanansarisvvv@gmail.com>
2026-02-20 13:42:52 +0530

    Fix ALLOWED_HOSTS and CSRF for Render

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -36,6 +36,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-default-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 
 
